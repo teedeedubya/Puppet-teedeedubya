8b16d7ad954a59640193ded1fdd1fef55f3ec5ed
Tony Welder <tony.wvoip@gmail.com>
2015-05-06 17:32:03 -0500

    adding module to install open vmtools

---
+++ modules/vmtools/manifests/init.pp
@@ -0,0 +1,28 @@
+
+#
+# Configures VM Tools
+#
+class vmtools {
+
+  require yum
+
+  # Remove proprietary VMware Tools, if they are already installed,
+  # and install open-vm-tools RPM from the EPEL repository
+  if ($virtual == 'vmware'){
+    exec {"remove-vmware-tools":
+      command => '/usr/bin/vmware-uninstall-tools.pl',
+      onlyif  => '/usr/bin/test -e /usr/bin/vmware-uninstall-tools.pl',
+	  timeout => 600,
+    }
+    ->
+    package { "open-vm-tools":
+	  ensure  => 'installed',
+	}
+    ->
+    service { "vmtoolsd":
+      ensure     => running,
+      enable     => true,
+      hasrestart => true
+    }  
+  }
+}
