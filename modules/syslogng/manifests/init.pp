1af0e79ea709e8b922d4793442769ccb8c0b10ba
Tony Welder <tony.wvoip@gmail.com>
2015-05-05 22:23:23 -0500

    adding a simple syslog-ng module

---
+++ modules/syslogng/manifests/init.pp
@@ -0,0 +1,36 @@
+
+#
+# Configures syslog-ng
+#
+class syslogng (
+  $enabled = "true",
+  $transport_logs = "false", 
+  $transport_protocol = "udp", #valid options are tcp or udp
+  $transport_destination = "127.0.0.1", #default is not a valid option
+  $transport_port = "22551"
+  ) {
+  if $enabled == "true" {
+    require dns_client
+    require yum
+  
+    package { 'syslog-ng': ensure => 'installed', } ->
+    package { 'syslog-ng-libdbi': ensure => 'installed', } ->
+    service { "rsyslog":
+      enable     => false,
+      ensure     => stopped,
+    } ->
+    file { '/etc/syslog-ng/syslog-ng.conf':
+      ensure  => present,
+	  owner   => root,
+	  group   => root, 
+      mode    => 0644,
+      notify  => Service[syslog-ng],
+      content => template("syslogng/syslog-ng.conf.erb"),
+    } ~>
+    service { "syslog-ng":
+      enable     => true,
+      ensure     => running,
+      hasrestart => true,
+    }
+  }
+}
